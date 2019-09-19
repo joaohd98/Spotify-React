7673978c1555781ec8b8e87a41acef3264557d7b
Joao Damazio <joaodamazio98@hotmail.com>
2019-09-19 14:38:15 -0300

    Adicionado typescript e sass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
+import {Layout} from "./layout/layout";
 
-ReactDOM.render(<div> Hello World</div>, document.getElementById('app'));
+ReactDOM.render(<Layout/>, document.getElementById('app'));
---
+++ src/layout/layout.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const Layout = () => {
+
+  return (
+    <div>Hello World</div>
+  );
+
+};
