46115c1a43357962b8fe21b89630480b162d8481
Joao Damazio <joaodamazio98@hotmail.com>
2019-09-19 16:01:50 -0300

    Adicionado rotas com redux

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
-import './layout.scss'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 import store from "../redux/store";
+import './layout.scss'
+import {SearchAlbumMusic} from "../pages/search-album-music/search-album-music";
 
 export const Layout = () => {
 
   return (
     <Provider store={store()}>
-      <div>
-        Hello world
-      </div>
+      <BrowserRouter>
+        <Route path="/" exact component={SearchAlbumMusic} />
+      </BrowserRouter>
     </Provider>
   );
 
---
+++ src/pages/search-album-music/search-album-music.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class SearchAlbumMusic extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        Hello SearchAlbumMusic
+      </div>
+    )
+  }
+
+}
