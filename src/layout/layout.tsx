42bd8f2dc4ffa184623c3c6eae9ccb3389055ca3
Joao Damazio <joaodamazio98@hotmail.com>
2019-09-19 15:30:20 -0300

    Adicionado redux sem reducers

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import './layout.scss'
+import { Provider } from 'react-redux'
+import store from "../redux/store";
 
 export const Layout = () => {
 
   return (
-    <div>Hello World</div>
+    <Provider store={store()}>
+      <div>
+        Hello world
+      </div>
+    </Provider>
   );
 
 };
+
