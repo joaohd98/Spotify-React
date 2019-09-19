e6b00155fee690ac37dbbdee1940f8af9faf25b8
Joao Damazio <joaodamazio98@hotmail.com>
2019-09-19 14:53:49 -0300

    Arrumado local do css global

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import {Layout} from "./layout/layout";
 
 ReactDOM.render(<Layout/>, document.getElementById('app'));
