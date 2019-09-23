502609fe988c15562df7593ae6d22890dab9f13d
Joao Damazio <joaodamazio98@hotmail.com>
2019-09-22 22:50:00 -0300

    Adicionado persist do redux

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,25 +1,30 @@
 import * as React from 'react';
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux'
 import { BrowserRouter, Route } from 'react-router-dom'
 import store from "../redux/store";
 import './styles.scss'
 import logo from "../assets/logo.png"
 import {AlbumsPage} from "../pages/albums";
+import {PersistGate} from "redux-persist/integration/react";
+import {persistStore} from "redux-persist";
+import {UserModel} from "../user/user-model";
 
-export const Layout = () => {
+export const Layout = (props: UserModel) => {
 
-
+  console.log(props);
 
   return (
     <Provider store={store()}>
-      <BrowserRouter>
-        <div className="container">
-          <img className="logo" src={logo} alt="logo"/>
-          <div className="page-container">
-            <Route path="/" exact component={AlbumsPage} />
+      <PersistGate loading={null} persistor={persistStore(store())}>
+        <BrowserRouter>
+          <div className="container">
+            <img className="logo" src={logo} alt="logo"/>
+            <div className="page-container">
+              <Route path="/" exact component={AlbumsPage} />
+            </div>
           </div>
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 
--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -2,14 +2,20 @@ import {combineReducers} from "redux";
 import {AlbumsPageModel} from "../pages/albums/services/albums-page-model";
 import {UserReducer} from "../user/user-reducer";
 import {AlbumsReducer} from "../pages/albums/redux/albums-reducer";
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import {UserModel} from "../user/user-model"; // defaults to localStorage for web
 
 export interface StatesReducers {
   AlbumsReducer: AlbumsPageModel.Props,
+  UserReducer: UserModel,
 }
+
+const UserPersistedReducer = persistReducer({ key: 'root', storage}, UserReducer);
 
 let States = {
   AlbumsReducer,
-  UserReducer
+  UserPersistedReducer
 };
 
 export const combinedReducers = () => combineReducers(States);
