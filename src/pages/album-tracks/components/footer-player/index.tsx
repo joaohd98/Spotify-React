79f3eca401ce796ec11782d4ab591dfe7f4e187d
Joao Damazio <joaodamazio98@hotmail.com>
2019-09-24 16:47:49 -0300

    Adicionado player

--- src/pages/album-tracks/components/footer-player/index.tsx
+++ src/pages/album-tracks/components/footer-player/index.tsx
@@ -6,8 +6,26 @@ export class FooterPlayer extends React.Component {
   render() {
 
     return (
-      <div>
-
+      <div className="player">
+        <div className="progress-bar"/>
+        <div className="player-actions">
+          <div className="previous-next">
+            <button>
+              <i className="custom-icon previous-player" />
+            </button>
+            <button>
+              <i className="custom-icon next-player" />
+            </button>
+          </div>
+          <div className="title">
+            Titulo da musica
+          </div>
+          <div className="play-pause">
+            <button>
+              <i className="custom-icon play" />
+            </button>
+          </div>
+        </div>
       </div>
     )
 
