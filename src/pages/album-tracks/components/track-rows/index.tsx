52a5eda03563dd8986b770fb3ece12ec834765ad
Joao Damazio <joaodamazio98@hotmail.com>
2019-09-24 13:49:50 -0300

    Adicionado componente para musica

--- src/pages/album-tracks/components/track-rows/index.tsx
+++ src/pages/album-tracks/components/track-rows/index.tsx
@@ -6,8 +6,20 @@ export class TrackRows extends React.Component {
   render() {
 
     return (
-      <div>
-
+      <div className="track-list">
+        <p className="track-row">
+          <span className="track-number">
+            <span>
+              1.
+            </span>
+            <span>
+              Nome da faixa
+            </span>
+          </span>
+          <span className="duration">
+            2:32
+          </span>
+        </p>
       </div>
     )
 
