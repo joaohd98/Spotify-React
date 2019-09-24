6c1733257cef12902a89b2665249340ec39c0b58
Joao Damazio <joaodamazio98@hotmail.com>
2019-09-24 18:41:54 -0300

    Adicionado responsive na musica

--- src/pages/album-tracks/components/track-rows/index.tsx
+++ src/pages/album-tracks/components/track-rows/index.tsx
@@ -3,23 +3,39 @@ import './styles.scss'
 
 export class TrackRows extends React.Component {
 
-  render() {
+  getNumber = () => {
 
-    return (
-      <div className="track-list">
+    let elements: JSX.Element[] = [];
+
+    for(let i = 1; i <= 10; i++){
+
+      elements.push(
         <p className="track-row">
           <span className="track-number">
             <span>
-              1.
+              { i }.
             </span>
             <span>
-              Nome da faixa
+              nome da musica
             </span>
           </span>
           <span className="duration">
             2:32
           </span>
         </p>
+      )
+
+    }
+
+    return elements;
+
+  };
+
+  render() {
+
+    return (
+      <div className="track-list">
+        { this.getNumber() }
       </div>
     )
 
